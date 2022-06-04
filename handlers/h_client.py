13fecdde94d0b109b7da1f5c85c407623eca6ebf
aleksei-motin <inbox@motin.info>
2022-06-04 10:15:04 +0300

    bot for timetable is ready

--- handlers/h_client.py
+++ handlers/h_client.py
@@ -1,5 +1,4 @@
 from aiogram import types
-from datetime import date
 import database
 from imports import dp
 from keyboards import kb_client
@@ -12,10 +11,10 @@ async def start(message: types.Message):
                          reply_markup=kb_client)
 
 
-async def get_timetable_today(message: types.Message):
+async def get_timetable_for_today(message: types.Message):
     today = dt.date.today().strftime('%d/%m')
-    result = database.db_sqlite.Database('database/db.db').sql_get_timetable_by_date(today)
-    print(result)
+    result = database.db_sqlite.Database('database/db.db').\
+        sql_get_timetable_by_date(today)
     res = ''
     for i in result:
         row = ' | '.join(i[1:])
@@ -23,11 +22,40 @@ async def get_timetable_today(message: types.Message):
     await message.answer(res)
 
 
+async def get_timetable_for_tomorrow(message: types.Message):
+    tomorrow = (dt.date.today() + dt.timedelta(days=1)).strftime('%d/%m')
+    result = database.db_sqlite.Database('database/db.db').\
+        sql_get_timetable_by_date(tomorrow)
+    if result == []:
+        await message.answer("Пар на завтра нет. Скорее всего завтра воскресенье.")
+    else:
+        res = ''
+        for i in result:
+            row = ' | '.join(i[1:])
+            res += f'\n\n{row}'
+        await message.answer(res)
+
+
 async def get_all_timetable(message: types.Message):
     result = database.db_sqlite.Database('database/db.db').sql_get_all()
-    await message.reply(f"Расписание на сессию:\n{result}")
+    long = len(result)
+    half = int(long / 2)
+    part_1 = result[:half]
+    res_1 = ''
+    for i in part_1:
+        row = ' | '.join(i)
+        res_1 += f'\n\n{row}'
+    part_2 = result[half:]
+    res_2 = ''
+    for i in part_2:
+        row = ' | '.join(i)
+        res_2 += f'\n\n{row}'
+    await message.reply(f"Расписание на сессию 1 часть:\n{res_1}")
+    await message.reply(f"Расписание на сессию 2 часть:\n{res_2}")
+
 
 def register_handlers_for_client():
     dp.register_message_handler(start, commands=['start'])
-    dp.register_message_handler(get_timetable_today, commands=['Расписание_на_сегодня'])
+    dp.register_message_handler(get_timetable_for_today, commands=['Расписание_на_сегодня'])
+    dp.register_message_handler(get_timetable_for_tomorrow, commands=['Расписание_на_завтра'])
     dp.register_message_handler(get_all_timetable, commands=['Все_расписание'])
--- keyboards/kb_client.py
+++ keyboards/kb_client.py
@@ -1,9 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 button_1 = KeyboardButton('/Расписание_на_сегодня')
-button_2 = KeyboardButton('/Все_расписание')
-# button_3 = KeyboardButton('/Ответы')
+button_2 = KeyboardButton('/Расписание_на_завтра')
+button_3 = KeyboardButton('/Все_расписание')
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb_client.add(button_1).add(button_2)#.insert(button_3)
+kb_client.add(button_1).insert(button_2).add(button_3)
